b3de106c8eb5ae8b2e65d65fe36488d8b904aafe
nullptr <kashifasif.cs@gmail.com>
2026-07-02 21:41:35 +0500

    miniproject

---
+++ Project03/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+path = require('path');
+const userModel = require('./models/user');
+const postModel = require('./models/post');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+
+app.set('view engine','ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(express.static(path.join(__dirname,'public')));
+app.use(cookieParser());
+
+
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+app.post('/register',async (req,res)=>{
+     const {name,username,password,email,age} = req.body;
+
+    let user = await userModel.findOne({email});
+    if(user) return res.status(500).send('User already registered');
+
+    bcrypt.genSalt(10,(err,salt) =>{
+        bcrypt.hash(password, salt ,async (err,hash) =>{
+           const user = await userModel.create({
+                name,
+                username,
+                email,
+                password : hash,
+                age
+            });
+
+            const token = jwt.sign({email:email, userid : user._id},"shhhhh");
+            res.cookie("token" , token);
+            res.send("registered")
+        });
+    });
+
+});
+
+
+app.listen(3000);
