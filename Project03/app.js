508abc337c6612e1e4c8ff296451d17f900cc44d
nullptr <kashifasif.cs@gmail.com>
2026-07-02 22:39:24 +0500

    miniproject

--- Project03/app.js
+++ Project03/app.js
@@ -17,7 +17,12 @@ app.use(cookieParser());
 app.get('/',(req,res)=>{
     res.render('index');
 });
-
+app.get('/login',(req,res)=>{
+    res.render('login');
+});
+app.get('/profile',isLogIn,(req,res)=>{
+    res.render('login');
+});
 app.post('/register',async (req,res)=>{
      const {name,username,password,email,age} = req.body;
 
@@ -41,6 +46,55 @@ app.post('/register',async (req,res)=>{
     });
 
 });
+
+app.post('/login',async (req,res)=>{
+     const {name,username,password,email,age} = req.body;
+
+    let user = await userModel.findOne({email});
+    if(!user) return res.status(500).send('Something went wrong');
+
+    bcrypt.compare(password,user.password,(err,result)=>{
+        if(result){
+            const token = jwt.sign({email:email, userid : user._id},"shhhhh");
+            res.cookie("token" , token);
+            return res.send("login successful");
+        }
+        else return res.redirect('/login');
+    })
+
+    
+
+});
+
+app.post('/login',async (req,res)=>{
+     const {name,username,password,email,age} = req.body;
+
+    let user = await userModel.findOne({email});
+    if(!user) return res.status(500).send('Something went wrong');
+
+    bcrypt.compare(password,user.password,(err,result)=>{
+        if(result) return res.send("login successful");
+        else return res.redirect('/login');
+    })
+
+    
+
+});
+
+app.get('/logout',(req,res)=>{
+    res.cookie("token","");
+    res.redirect('/login'); 
+
+});
+
+function isLogIn(req,res,next){
+    if(req.cookies.token === "") return res.send('you must be login');
+    else{
+         const data = jwt.verify(req.cookies.token,"shhhhh");
+         req.user = data;
+         next();
+    }
+}
 
 
 app.listen(3000);
